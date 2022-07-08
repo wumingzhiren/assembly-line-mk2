832d932cbbf2c108fd3ebd9253ba105357db5ec3
wumingzhiren <wang_hong_da@126.com>
2022-07-08 23:57:40 +0800

    补充各级XX电路及橡胶环和箔

--- src/recipe/chipDatabase.lua
+++ src/recipe/chipDatabase.lua
@@ -1,128 +1,162 @@
 chipDatabase = {}
 
+chipDatabase['任意EV电路板'] = 'ev'
 chipDatabase['工作站'] = 'ev'
 chipDatabase['量子处理器'] = 'ev'
 chipDatabase['纳米处理器集群'] = 'ev'
 chipDatabase['数据磁共振电路'] = 'ev'
 
+chipDatabase['dreamcraft:item.CircuitEV.0'] = 'ev'
 chipDatabase['gregtech:gt.metaitem.01.32704'] = 'ev'
 chipDatabase['gregtech:gt.metaitem.03.32085'] = 'ev'
 chipDatabase['gregtech:gt.metaitem.03.32083'] = 'ev'
 chipDatabase['bartworks:gt.bwMetaGeneratedItem0.8'] = 'ev'
 
+chipDatabase['任意IV电路板'] = 'iv'
 chipDatabase['处理器主机'] = 'iv'
 chipDatabase['精英纳米电脑'] = 'iv'
 chipDatabase['量子处理器集群'] = 'iv'
 chipDatabase['晶体处理器'] = 'iv'
 chipDatabase['精英磁共振电路'] = 'iv'
 
+chipDatabase['dreamcraft:item.CircuitIV.0'] = 'iv'
 chipDatabase['gregtech:gt.metaitem.01.32705'] = 'iv'
 chipDatabase['gregtech:gt.metaitem.03.32084'] = 'iv'
 chipDatabase['gregtech:gt.metaitem.03.32086'] = 'iv'
 chipDatabase['gregtech:gt.metaitem.03.32089'] = 'iv'
 chipDatabase['bartworks:gt.bwMetaGeneratedItem0.9'] = 'iv'
 
+chipDatabase['任意LuV电路板'] = 'luv'
 chipDatabase['纳米处理器主机'] = 'luv'
 chipDatabase['大型量子电脑'] = 'luv'
 chipDatabase['湿件处理器'] = 'luv'
 chipDatabase['晶体处理器集群'] = 'luv'
 chipDatabase['大师磁共振电路'] = 'luv'
 
+chipDatabase['dreamcraft:item.CircuitLuV.0'] = 'luv'
 chipDatabase['gregtech:gt.metaitem.01.32706'] = 'luv'
 chipDatabase['gregtech:gt.metaitem.03.32087'] = 'luv'
 chipDatabase['gregtech:gt.metaitem.03.32092'] = 'luv'
 chipDatabase['gregtech:gt.metaitem.03.32096'] = 'luv'
 chipDatabase['bartworks:gt.bwMetaGeneratedItem0.10'] = 'luv'
 
+chipDatabase['任意ZPM电路板'] = 'zpm'
 chipDatabase['量子处理器主机'] = 'zpm'
 chipDatabase['终极晶体电脑'] = 'zpm'
 chipDatabase['湿件处理器集群'] = 'zpm'
 chipDatabase['生物处理器'] = 'zpm'
 chipDatabase['顶级磁共振电路'] = 'zpm'
 
+chipDatabase['dreamcraft:item.CircuitZPM.0'] = 'zpm'
 chipDatabase['gregtech:gt.metaitem.03.32088'] = 'zpm'
 chipDatabase['gregtech:gt.metaitem.03.32090'] = 'zpm'
 chipDatabase['gregtech:gt.metaitem.03.32093'] = 'zpm'
 chipDatabase['gregtech:gt.metaitem.03.32097'] = 'zpm'
 chipDatabase['bartworks:gt.bwMetaGeneratedItem0.11'] = 'zpm'
 
+chipDatabase['任意UV电路板'] = 'uv'
 chipDatabase['晶体处理器主机'] = 'uv'
 chipDatabase['湿件处理器超级电脑'] = 'uv'
 chipDatabase['生物处理器集群'] = 'uv'
 chipDatabase['光学处理器'] = 'uv'
 chipDatabase['超导磁共振电路'] = 'uv'
 
+chipDatabase['dreamcraft:item.CircuitUV.0'] = 'uv'
 chipDatabase['gregtech:gt.metaitem.03.32091'] = 'uv'
 chipDatabase['gregtech:gt.metaitem.03.32094'] = 'uv'
 chipDatabase['gregtech:gt.metaitem.03.32098'] = 'uv'
 chipDatabase['gregtech:gt.metaitem.03.32154'] = 'uv'
 chipDatabase['bartworks:gt.bwMetaGeneratedItem0.12'] = 'uv'
 
+chipDatabase['任意UHV电路板'] = 'uhv'
 chipDatabase['湿件处理器主机'] = 'uhv'
 chipDatabase['生物处理器超级电脑'] = 'uhv'
 chipDatabase['光学处理器集群'] = 'uhv'
 chipDatabase['天顶星处理器'] = 'uhv'
 chipDatabase['终极磁共振电路'] = 'uhv'
 
+chipDatabase['dreamcraft:item.CircuitUHV.0'] = 'uhv'
 chipDatabase['gregtech:gt.metaitem.03.32095'] = 'uhv'
 chipDatabase['gregtech:gt.metaitem.03.32099'] = 'uhv'
 chipDatabase['gregtech:gt.metaitem.03.32155'] = 'uhv'
 chipDatabase['gregtech:gt.metaitem.03.3216'] = 'uhv'
 chipDatabase['bartworks:gt.bwMetaGeneratedItem0.13'] = 'uhv'
 
+chipDatabase['任意UEV电路板'] = 'uev'
 chipDatabase['光学处理器超级电脑'] = 'uev'
 chipDatabase['生物处理器主机'] = 'uev'
 chipDatabase['天顶星处理器集群'] = 'uev'
 chipDatabase['寰宇处理器'] = 'uev'
 chipDatabase['生物磁共振电路'] = 'uev'
 
+chipDatabase['dreamcraft:item.CircuitUEV.0'] = 'uev'
 chipDatabase['gregtech:gt.metaitem.03.32120'] = 'uev'
 chipDatabase['gregtech:gt.metaitem.03.32156'] = 'uev'
 chipDatabase['gregtech:gt.metaitem.03.32167'] = 'uev'
 chipDatabase['gregtech:gt.metaitem.03.32170'] = 'uev'
 chipDatabase['bartworks:gt.bwMetaGeneratedItem0.14'] = 'uev'
 
+chipDatabase['任意UIV电路板'] = 'uiv'
 chipDatabase['纳米电路'] = 'uiv'
 chipDatabase['光学处理器主机'] = 'uiv'
 chipDatabase['天顶星处理器超级电脑'] = 'uiv'
 chipDatabase['寰宇处理器集群'] = 'uiv'
 chipDatabase['超时空处理器'] = 'uiv'
 
+chipDatabase['dreamcraft:item.CircuitUIV.0'] = 'uiv'
 chipDatabase['dreamcraft:item.NanoCircuit.0'] = 'uiv'
 chipDatabase['gregtech:gt.metaitem.03.32157'] = 'uiv'
 chipDatabase['gregtech:gt.metaitem.03.32168'] = 'uiv'
 chipDatabase['gregtech:gt.metaitem.03.32171'] = 'uiv'
 chipDatabase['gregtech:gt.metaitem.03.32174'] = 'uiv'
 
+chipDatabase['任意UMV电路板'] = 'umv'
 chipDatabase['Pico电路'] = 'umv'
 chipDatabase['天顶星处理器主机'] = 'umv'
 chipDatabase['寰宇处理器超级电脑'] = 'umv'
 chipDatabase['超时空处理器集群'] = 'umv'
 
+chipDatabase['dreamcraft:item.CircuitUMV.0'] = 'umv'
 chipDatabase['dreamcraft:item.PikoCircuit.0'] = 'umv'
 chipDatabase['gregtech:gt.metaitem.03.32169'] = 'umv'
 chipDatabase['gregtech:gt.metaitem.03.32172'] = 'umv'
 chipDatabase['gregtech:gt.metaitem.03.32175'] = 'umv'
 
+chipDatabase['任意UXV电路板'] = 'uxv'
 chipDatabase['量子电路'] = 'uxv'
 chipDatabase['寰宇处理器主机'] = 'uxv'
 chipDatabase['超时空处理器超级电脑'] = 'uxv'
 
+chipDatabase['dreamcraft:item.CircuitUXV.0'] = 'uxv'
 chipDatabase['dreamcraft:item.QuantumCircuit.0'] = 'uxv'
 chipDatabase['gregtech:gt.metaitem.03.32173'] = 'uxv'
 chipDatabase['gregtech:gt.metaitem.03.32176'] = 'uxv'
 
+chipDatabase['任意MAX电路板'] = 'max'
 chipDatabase['超时空处理器主机'] = 'max'
 
+chipDatabase['dreamcraft:item.CircuitMAX.0'] = 'max'
 chipDatabase['gregtech:gt.metaitem.03.32177'] = 'max'
 
-chipDatabase['PTMEG橡胶板'] = 'rabberPlate'
-chipDatabase['丁苯橡胶片'] = 'rabberPlate'
-chipDatabase['硅橡胶片'] = 'rabberPlate'
+chipDatabase['PTMEG橡胶板'] = 'rubberSheet'
+chipDatabase['丁苯橡胶片'] = 'rubberSheet'
+chipDatabase['硅橡胶片'] = 'rubberSheet'
 
-chipDatabase['bartworks:gt.bwMetaGeneratedplate.11312'] = 'rabberPlate'
-chipDatabase['gregtech:gt.metaitem.01.17471'] = 'rabberPlate'
-chipDatabase['gregtech:gt.metaitem.01.17635'] = 'rabberPlate'
+chipDatabase['bartworks:gt.bwMetaGeneratedplate.11312'] = 'rubberSheet'
+chipDatabase['gregtech:gt.metaitem.01.17471'] = 'rubberSheet'
+chipDatabase['gregtech:gt.metaitem.01.17635'] = 'rubberSheet'
+
+chipDatabase['丁苯橡胶环'] = 'rubberRing'
+chipDatabase['硅橡胶环'] = 'rubberRing'
+
+chipDatabase['gregtech:gt.metaitem.01.28471'] = 'rubberRing'
+chipDatabase['gregtech:gt.metaitem.01.28635'] = 'rubberRing'
+
+chipDatabase['薄丁苯橡胶片'] = 'rubberThinSheet'
+chipDatabase['薄硅橡胶片'] = 'rubberThinSheet'
+
+chipDatabase['gregtech:gt.metaitem.01.29471'] = 'rubberThinSheet'
+chipDatabase['gregtech:gt.metaitem.01.29635'] = 'rubberThinSheet'
 
 return chipDatabase
 
